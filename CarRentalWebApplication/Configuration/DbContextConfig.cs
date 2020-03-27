fa27cab113dd89a3e5b91f40db5d3f948848567f
Ivan Trashchenko <ivan.trashchenko@gmail.com>
2020-03-27 11:21:09 +0300

    Add authorization

---
+++ CarRentalWebApplication/Configuration/DbContextConfig.cs
@@ -0,0 +1,25 @@
+﻿using CarRentalWebApplication.Data;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarRentalWebApplication.Configuration
+{
+    public static class DbContextConfig
+    {
+        public static void ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var assemblyName = typeof(ApplicationDbContext).Namespace;
+            var connectionString = configuration["SqlConnectionConfig:ConnectionString"];
+            services.AddDbContext<ApplicationDbContext>(
+                o => o.UseSqlServer(
+                    connectionString,
+                    b =>
+                        {
+                            b.MigrationsAssembly(assemblyName);
+                            b.CommandTimeout(300);
+                        }));
+        }
+    }
+}
